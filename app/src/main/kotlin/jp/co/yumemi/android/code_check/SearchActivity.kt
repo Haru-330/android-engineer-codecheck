4781e4749db932eb60b7b3b3e7abeedcd7aa9636
Haru-330 <haruna.330romiyaneiro@gmail.com>
2023-04-03 21:02:16 +0900

    Add Comment about Class in SearchActivity.kt

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/SearchActivity.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/SearchActivity.kt
@@ -6,6 +6,10 @@ package jp.co.yumemi.android.code_check
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
+/**
+ * SearchActivity クラス
+ * GitHub リポジトリを検索するためのアクティビティを実装するクラスです。
+ */
 class SearchActivity : AppCompatActivity(R.layout.activity_search) {
 
     companion object {
