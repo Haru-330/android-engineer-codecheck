388d8f4cf5efd42ce09a324d9e3e836aeb34f92e
Haru-330 <haruna.330romiyaneiro@gmail.com>
2023-04-04 20:26:19 +0900

    Replace !! with ? and add null check

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/GitHubSearchViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/GitHubSearchViewModel.kt
@@ -40,36 +40,37 @@ class GitHubSearchViewModel(
         val client = HttpClient(Android)
 
         return@runBlocking GlobalScope.async {
-            val response: HttpResponse = client?.get("https://api.github.com/search/repositories") {
-                header("Accept", "application/vnd.github.v3+json")
-                parameter("q", inputText)
-            }
+            val response: HttpResponse? =
+                client?.get("https://api.github.com/search/repositories") {
+                    header("Accept", "application/vnd.github.v3+json")
+                    parameter("q", inputText)
+                }
 
-            val jsonBody = JSONObject(response.receive<String>())
+            val jsonBody = JSONObject(response?.receive<String>() ?: "")
 
-            val jsonItems = jsonBody.optJSONArray("items")!!
+            val jsonItems = jsonBody.optJSONArray("items")
 
             val items = mutableListOf<item>()
 
             for (i in 0 until jsonItems.length()) {
-                val jsonItem = jsonItems.optJSONObject(i)!!
-                val name = jsonItem.optString("full_name")
-                val ownerIconUrl = jsonItem.optJSONObject("owner")!!.optString("avatar_url")
-                val language = jsonItem.optString("language")
-                val stargazersCount = jsonItem.optLong("stargazers_count")
-                val watchersCount = jsonItem.optLong("watchers_count")
-                val forksCount = jsonItem.optLong("forks_count")
-                val openIssuesCount = jsonItem.optLong("open_issues_count")
+                val jsonItem = jsonItems.optJSONObject(i)
+                val name = jsonItem?.optString("full_name")
+                val ownerIconUrl = jsonItem?.optJSONObject("owner")?.optString("avatar_url")
+                val language = jsonItem?.optString("language")
+                val stargazersCount = jsonItem?.optLong("stargazers_count")
+                val watchersCount = jsonItem?.optLong("watchers_count")
+                val forksCount = jsonItem?.optLong("forks_count")
+                val openIssuesCount = jsonItem?.optLong("open_issues_count")
 
                 items.add(
                     item(
-                        name = name,
-                        ownerIconUrl = ownerIconUrl,
+                        name = name ?: "",
+                        ownerIconUrl = ownerIconUrl ?: "",
                         language = context.getString(R.string.written_language, language),
-                        stargazersCount = stargazersCount,
-                        watchersCount = watchersCount,
-                        forksCount = forksCount,
-                        openIssuesCount = openIssuesCount
+                        stargazersCount = stargazersCount ?: 0,
+                        watchersCount = watchersCount ?: 0,
+                        forksCount = forksCount ?: 0,
+                        openIssuesCount = openIssuesCount ?: 0
                     )
                 )
             }
