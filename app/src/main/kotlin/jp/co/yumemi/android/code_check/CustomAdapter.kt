05601493b82b31301c056ecc327549326de2a23a
Haru-330 <haruna.330romiyaneiro@gmail.com>
2023-04-07 16:11:43 +0900

    Separate CustomAdapter class file from SearchFragment

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/CustomAdapter.kt
@@ -0,0 +1,60 @@
+package jp.co.yumemi.android.code_check
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * CustomAdapter クラス
+ * @param Item
+ * RecyclerViewのListAdapterクラスを継承して、itemのリストを表示するためのカスタムアダプターです。
+ * アイテムがクリックされたときに、OnItemClickListenerインターフェースのitemClickメソッドを呼び出します。
+ */
+class CustomAdapter(
+    private val itemClickListener: OnItemClickListener,
+) : ListAdapter<Item, CustomAdapter.ViewHolder>(diffUtil) {
+    /**
+     * ViewHolderクラス
+     * @param view RecyclerViewで使用されるView
+     * カスタムアダプターのビューホルダーです。
+     */
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
+    /**
+     * OnItemClickListenerインターフェース
+     * itemがクリックされたときに呼び出されるメソッドを定義します。
+     */
+    interface OnItemClickListener {
+        fun itemClick(item: Item)
+    }
+
+    /**
+     * onCreateViewHolderメソッド
+     * @param parent RecyclerViewの親ViewGroup
+     * @param viewType ビュータイプ
+     * @return ViewHolder
+     * 新しいViewHolderインスタンスを作成します。
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    /**
+     * onBindViewHolderメソッド
+     * @param holder RecyclerViewのViewHolder
+     * @param position リストアイテムのポジション
+     * アイテムのデータをViewHolderのビューにバインドします。
+     */
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.itemView.findViewById<TextView>(R.id.repositoryNameView).text = item.name
+
+        holder.itemView.setOnClickListener {
+            itemClickListener.itemClick(item)
+        }
+    }
+}
--- app/src/main/kotlin/jp/co/yumemi/android/code_check/SearchFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/SearchFragment.kt
@@ -4,11 +4,8 @@
 package jp.co.yumemi.android.code_check
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
-import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -104,56 +101,4 @@ val diffUtil = object : DiffUtil.ItemCallback<Item>() {
         return oldItem == newItem
     }
 
-}
-
-/**
- * CustomAdapter クラス
- * @param Item
- * RecyclerViewのListAdapterクラスを継承して、itemのリストを表示するためのカスタムアダプターです。
- * アイテムがクリックされたときに、OnItemClickListenerインターフェースのitemClickメソッドを呼び出します。
- */
-class CustomAdapter(
-    private val itemClickListener: OnItemClickListener,
-) : ListAdapter<Item, CustomAdapter.ViewHolder>(diffUtil) {
-    /**
-     * ViewHolderクラス
-     * @param view RecyclerViewで使用されるView
-     * カスタムアダプターのビューホルダーです。
-     */
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
-
-    /**
-     * OnItemClickListenerインターフェース
-     * itemがクリックされたときに呼び出されるメソッドを定義します。
-     */
-    interface OnItemClickListener {
-        fun itemClick(item: Item)
-    }
-
-    /**
-     * onCreateViewHolderメソッド
-     * @param parent RecyclerViewの親ViewGroup
-     * @param viewType ビュータイプ
-     * @return ViewHolder
-     * 新しいViewHolderインスタンスを作成します。
-     */
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_item, parent, false)
-        return ViewHolder(view)
-    }
-
-    /**
-     * onBindViewHolderメソッド
-     * @param holder RecyclerViewのViewHolder
-     * @param position リストアイテムのポジション
-     * アイテムのデータをViewHolderのビューにバインドします。
-     */
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = getItem(position)
-        holder.itemView.findViewById<TextView>(R.id.repositoryNameView).text = item.name
-
-        holder.itemView.setOnClickListener {
-            itemClickListener.itemClick(item)
-        }
-    }
 }
