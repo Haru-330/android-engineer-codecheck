f19a2712c981756c93a7639e0dfd40d91db8659c
Haru-330 <haruna.330romiyaneiro@gmail.com>
2023-04-07 14:35:30 +0900

    Refactor exception handling in GitHubSearchViewMmodel

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/GitHubSearchViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/GitHubSearchViewModel.kt
@@ -4,6 +4,7 @@
 package jp.co.yumemi.android.code_check
 
 import android.content.Context
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import io.ktor.client.*
 import io.ktor.client.call.*
@@ -34,50 +35,50 @@ class GitHubSearchViewModel(
      * 検索キーワードを用いて、GitHub APIを呼び出し、該当するリポジトリの情報を取得します。
      * 取得した情報をitemクラスのオブジェクトに変換し、リストに格納して返却します。
      */
-    val client = HttpClient(Android)
     fun searchGithubRepositories(inputText: String): List<Item> = runBlocking {
+        val client = HttpClient(Android)
+        try {
+            val response: HttpResponse? =
+                client?.get("https://api.github.com/search/repositories") {
+                    header("Accept", "application/vnd.github.v3+json")
+                    parameter("q", inputText)
+                }
 
-        val response: HttpResponse? = client?.get("https://api.github.com/search/repositories") {
-            header("Accept", "application/vnd.github.v3+json")
-            parameter("q", inputText)
-        }
+            val jsonBody = JSONObject(response?.receive<String>() ?: "")
 
-        val jsonBody = JSONObject(response?.receive<String>() ?: "")
+            val jsonItems = jsonBody.optJSONArray("items")
 
-        val jsonItems = jsonBody.optJSONArray("items")
+            val items = mutableListOf<Item>()
 
-        val items = mutableListOf<Item>()
+            for (i in 0 until jsonItems.length()) {
+                val jsonItem = jsonItems.optJSONObject(i)
+                val name = jsonItem?.optString("full_name")
+                val ownerIconUrl = jsonItem?.optJSONObject("owner")?.optString("avatar_url")
+                val language = jsonItem?.optString("language")
+                val stargazersCount = jsonItem?.optLong("stargazers_count")
+                val watchersCount = jsonItem?.optLong("watchers_count")
+                val forksCount = jsonItem?.optLong("forks_count")
+                val openIssuesCount = jsonItem?.optLong("open_issues_count")
 
-        for (i in 0 until jsonItems.length()) {
-            val jsonItem = jsonItems.optJSONObject(i)
-            val name = jsonItem?.optString("full_name")
-            val ownerIconUrl = jsonItem?.optJSONObject("owner")?.optString("avatar_url")
-            val language = jsonItem?.optString("language")
-            val stargazersCount = jsonItem?.optLong("stargazers_count")
-            val watchersCount = jsonItem?.optLong("watchers_count")
-            val forksCount = jsonItem?.optLong("forks_count")
-            val openIssuesCount = jsonItem?.optLong("open_issues_count")
-
-            items.add(
-                Item(
-                    name = name ?: "",
-                    ownerIconUrl = ownerIconUrl ?: "",
-                    language = context.getString(R.string.written_language, language),
-                    stargazersCount = stargazersCount ?: 0,
-                    watchersCount = watchersCount ?: 0,
-                    forksCount = forksCount ?: 0,
-                    openIssuesCount = openIssuesCount ?: 0
+                items.add(
+                    Item(
+                        name = name ?: "",
+                        ownerIconUrl = ownerIconUrl ?: "",
+                        language = context.getString(R.string.written_language, language),
+                        stargazersCount = stargazersCount ?: 0,
+                        watchersCount = watchersCount ?: 0,
+                        forksCount = forksCount ?: 0,
+                        openIssuesCount = openIssuesCount ?: 0
+                    )
                 )
-            )
+            }
+
+            lastSearchDate = Date()
+
+            return@runBlocking items.toList()
+        } catch (e: Exception) {
+            Log.e("GitHubSearchViewModel", "Failed to get search results", e)
+            return@runBlocking emptyList()
         }
-
-        lastSearchDate = Date()
-
-        return@runBlocking items.toList()
-    }
-
-    override fun onCleared() {
-        super.onCleared()
-        client.close()
     }
 }
